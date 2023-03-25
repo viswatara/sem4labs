dcef78d35adfe18b7ba9517314eb6bc7d417139e
viswatara <99180943+viswatara@users.noreply.github.com>
2023-03-25 10:31:16 +0530

    Add files via upload

---
+++ eslab/lab6/l6q2.c
@@ -0,0 +1,29 @@
+#include<LPC17xx.h>
+
+unsigned int j;
+unsigned long LED = 0x00000010;
+
+int main(void)
+{
+	LPC_PINCON->PINSEL0 &= 0xFF0000FF;
+	LPC_GPIO0->FIODIR |= 0X00000FF0;
+	LPC_PINCON->PINSEL0 &= 0xFCFFFFFF;
+	LPC_GPIO2->FIODIR &= 0XFFFFEFFF;
+  
+	
+		while(1)
+		{
+			if(LPC_GPIO2->FIOPIN&1<<12){
+				for(LED=255;LED>=0;LED--){
+					LPC_GPIO0->FIOPIN=LED<<4;
+					for(j=0;j<200000;j++);
+				}
+			}
+			else{
+			for(LED=0;LED<256;LED++){
+				LPC_GPIO0->FIOPIN=LED<<4;
+				for(j=0;j<200000;j++);
+			}
+			}
+	}
+}
