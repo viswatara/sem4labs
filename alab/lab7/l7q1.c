430487aaeb5cc8e200a7b87d29f21aa761d4794b
viswatara <99180943+viswatara@users.noreply.github.com>
2023-04-06 10:21:45 +0530

    Update l7q1.c

--- alab/lab7/l7q1.c
+++ alab/lab7/l7q1.c
@@ -1,80 +1,98 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define MAX(a,b) ((a)>(b) ? a : b)
 
-typedef struct node {
-	int info;
-	struct node *left,*right;
-}NODE;
 
-NODE* create(NODE *bnode,int x) {
-	NODE *getnode;
-	if(bnode==NULL) {
-		bnode=(NODE*) malloc(sizeof(NODE));
-		bnode->info=x;
-		bnode->left=bnode->right=NULL;
-	}
-	else if(x>bnode->info)
-		bnode->right=create(bnode->right,x);
-	else if(x<bnode->info)
-		bnode->left=create(bnode->left,x);
-	else {
-		printf("Duplicate node\n");
-		exit(0);
-	}
-	return(bnode);
+typedef struct node{
+    int val;
+    struct node *left;
+    struct node *right;
+}*NODE;
+
+NODE insert(NODE root,int x){
+    if(root==NULL){
+        root=(NODE)malloc(sizeof(struct node));
+        root->val=x;
+        root->left=root->right=NULL;
+    }
+    else if(x>root->val)
+        root->right=insert(root->right,x);
+    else if(x<root->val)
+        root->left=insert(root->left,x);
+    else{
+        printf("Duplicate node\n");
+        exit(0);
+    }
+    return(root);
 }
 
-void inorder(NODE *ptr){
-	if(ptr!=NULL){
-		inorder(ptr->left);
-		printf("%5d",ptr->info);
-		inorder(ptr->right);
-	}
+void postorder(NODE cur){
+    if(cur){
+        postorder(cur->left);
+        postorder(cur->right);
+        printf("%4d",cur->val);
+    }
 }
 
-void postorder(NODE *ptr){
-	if(ptr!=NULL) {
-		postorder(ptr->left);
-		postorder(ptr->right);
-		printf("%5d",ptr->info);
-	}
+void preorder(NODE cur){
+    if(cur){
+        printf("%4d",cur->val);
+        preorder(cur->left);
+        preorder(cur->right);
+    }
 }
 
-void preorder(NODE *ptr){
-	if(ptr!=NULL) {
-		printf("%5d",ptr->info);
-		preorder(ptr->left);
-		preorder(ptr->right);
-	}
+void inorder(NODE cur){
+    if(cur){
+        inorder(cur->left);
+        printf("%4d",cur->val);
+        inorder(cur->right);
+    }
 }
 
-void main(){
-	int n,x,ch,i;
-	NODE *root;
-	root=NULL;
-	while(1){		
-		printf("********************Output********************\n\n");
-		printf("-----------Menu-----------\n");
-		printf(" 1. Insert\n 2. All traversals\n 3. Exit\n");
-		printf("Enter your choice:");
-		scanf("%d",&ch);
-		switch(ch){
-			case 1: 
-				printf("Enter node (do not enter duplicate nodes):\n");
-				scanf("%d",&x);
-				root=create(root,x);
-				break;
-			case 2: 
-				printf("\nInorder traversal:\n");
-				inorder(root);
-				printf("\nPreorder traversal:\n");
-				preorder(root);
-				printf("\nPostorder traversal:\n");
-				postorder(root);
-				printf("\n\n*********************************************");
-				break;
-			case 3: 
-				exit(0);
-		}
-	}			
+int height(NODE cur){
+    if (cur == NULL)
+        return -1;
+    else
+        return MAX(height(cur->left),height(cur->right))+1;
+}
+
+void balancefactor(NODE cur){
+    static int x;
+    if(cur){
+        balancefactor(cur->left);
+        x = height(cur->left)-height(cur->right);
+        printf("\nNode with value %d has a balance factor of %d",cur->val,x);
+        balancefactor(cur->right);
+    }
+}
+
+int main(){
+    NODE root = NULL;
+    int ch,x;
+    do{
+        printf("\n1.Enter element(no duplicates)  2. Print elements  3. Show balance factor  4.Exit    Enter choice : ");
+        scanf("%d",&ch);
+        switch (ch){
+            case 1 : printf("Enter element : ");
+                     scanf("%d",&x);
+                     root = insert(root,x);
+                     break;
+            case 2 : printf("\nInorder traversal is : ");
+                     inorder(root);
+                     printf("\nPreorder traversal is : ");
+                     preorder(root);
+                     printf("\nPostorder traversal is : ");
+                     postorder(root);
+                     break;
+            case 3 : balancefactor(root);
+                     break;
+            
+            case 4 : break;
+
+            default:
+                break;
+        }
+    }while(ch != 4);
+    return 0;
 }
