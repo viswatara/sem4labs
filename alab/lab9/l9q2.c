8167e992714cf9be5bb88a46e742ded1158a0343
viswatara <99180943+viswatara@users.noreply.github.com>
2023-04-13 09:35:08 +0530

    Create l9q2.c

---
+++ alab/lab9/l9q2.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void initialize(int **hash_table,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            hash_table[i][j] = 0;
+        }
+    }
+
+}
+
+void Open_Hashing(int *arr,int **hash_table,int n)
+{
+    initialize(hash_table,n);
+    int m = n;
+    int k = 0;
+    for(int i=0;i<n;i++)
+    {
+        if(hash_table[arr[i] % m][0] != 0)
+            hash_table[arr[i]%m][++k] = arr[i];
+        else
+            hash_table[arr[i] % m][0] = arr[i];
+    }
+    for(int i= 0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(hash_table[i][j] != 0 || j == 0)
+                printf("%d ",hash_table[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void search(int **hash_table,int n,int searchele)
+{
+    int i = searchele % n;
+    for(int j=0;j<n;j++)
+    {
+        if(hash_table[i][j] == searchele)
+        {
+            printf("element found");
+            return;
+        }
+    }
+    printf("element not found");
+}
+
+int main()
+{
+    int n,searchele;
+    printf("enter the mod function:");
+    scanf("%d",&n);
+
+    int *arr = (int*)malloc(n*sizeof(int));
+    
+    printf("\nenter the elements:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter element to search");
+    scanf("%d",&searchele);
+    int **hash_table = (int**)malloc(n*sizeof(int*));
+    
+    for(int i=0;i<n;i++)
+    {
+        hash_table[i] = (int*)malloc(n*sizeof(int));
+    }
+    printf("The hash table is:\n");
+    Open_Hashing(arr,hash_table,n);
+    search(hash_table,n,searchele);
+}
