8049fb19a2d2d5c0505a62c4914d1e010b9ea8bd
viswatara <99180943+viswatara@users.noreply.github.com>
2023-04-16 19:30:48 +0530

    Update l6q1.c

--- eslab/lab6/l6q1.c
+++ eslab/lab6/l6q1.c
@@ -1,19 +1,23 @@
-#include<LPC17xx.h>
 
-unsigned int j;
-unsigned long LED = 0x00000010;
-
+#include <LPC17xx.h>
+unsigned int i,j; 
+unsigned long LED = 0x10;
 int main(void)
 {
+	SystemInit(); //Add these two function for its internal operation
+	SystemCoreClockUpdate();
 	LPC_PINCON->PINSEL0 &= 0xFF0000FF;
-	LPC_GPIO0->FIODIR |= 0X00000FF0;
-  
-	
+	//Configure Port0 PINS P0.4-P0.11 as GPIO function
+	LPC_GPIO0->FIODIR |= 0xFF0;
+	//Configure P0.4-P0.11 as output port 
 	while(1)
-	{
-		for(LED=0;LED<256;LED++){
-			LPC_GPIO0->FIOPIN=LED<<4;
-			for(j=0;j<200000;j++);
-	  }
-  }
+ {
+		LED = 0x10; // Initial value on LED 
+		for(i=1;i<256;i++)
+		{
+			LPC_GPIO0->FIOPIN = LED; 
+			for(j=0;j<50000;j++); //delay
+			LED += 0x10; 
+		}
+	}
 }
